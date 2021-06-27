d099e509c1ffff70e7e0e15717ae3d4026dcb63f
Carlton Brady <ctombrady@gmail.com>
2021-06-27 18:44:30 -0400

    Adding navigation bar header

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about/about.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { PostsComponent } from './components/posts/posts/posts.component';
+import { ProjectsComponent } from './components/projects/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/home',  pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
