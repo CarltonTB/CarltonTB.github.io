3c5fb9a0ce094085eea7d351d6202daae116282e
Carlton Brady <carltonbrady@Carltons-MacBook-Pro-2.local>
2022-09-30 00:21:35 -0400

    simple three js example

--- personal-site/src/main.ts
+++ personal-site/src/main.ts
@@ -1,23 +1,34 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// Scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, 
+  window.innerWidth / window.innerHeight, 0.1, 1000);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  // Rendered
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Geometry
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+// Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+
+camera.position.z = 5;
+
+const animate = () => {
+  requestAnimationFrame(animate)
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  controls.update();
+  renderer.render(scene, camera)
+}
+animate();
