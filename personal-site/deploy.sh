c29c9a21f917e520b7e4f34946ffe710d19993cb
Carlton Brady <carltonbrady@Carltons-MacBook-Pro-2.local>
2022-09-29 23:14:21 -0400

    update vite config

--- personal-site/deploy.sh
+++ personal-site/deploy.sh
@@ -15,6 +15,6 @@ echo > .nojekyll
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:CarltonTB/<CarltonTB>.github.io.git master
+git push -f git@github.com:CarltonTB/CarltonTB.github.io.git master
 
 cd -
