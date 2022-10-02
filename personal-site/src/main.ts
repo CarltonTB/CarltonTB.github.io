14d1a579a2330c62bd60eee6c6a1ad12640ed213
Carlton Brady <carltonbrady@carltons-mbp-2.myfiosgateway.com>
2022-10-02 15:21:40 -0400

    cube field

--- personal-site/src/main.ts
+++ personal-site/src/main.ts
@@ -1,34 +1,54 @@
 import './style.css'
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { FlyControls } from 'three/examples/jsm/controls/FlyControls';
 
 // Scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, 
   window.innerWidth / window.innerHeight, 0.1, 1000);
 
-  // Rendered
+  // Renderer
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Geometry
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const material = new THREE.MeshBasicMaterial({color: 0xffffff});
 
-// Controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.update();
+const cubes: THREE.Mesh[] = []
+// Add cubes throughout the scene
 
-camera.position.z = 5;
+const createCube = (x :number, y: number, z: number) => {
+  const cube = new THREE.Mesh( geometry, material );
+  cube.position.set(x, y, z)
+  scene.add( cube );
+  cubes.push(cube);
+}
+
+const createCubeField = () => {
+  for(let i = -100; i < 100; i+=10){
+    for(let j = -100; j < 100; j+=10) {
+      for(let k = 0; k > -200; k-=10) {
+        createCube(i, j, k);
+      }
+    }
+  }
+}
+
+createCubeField();
+
+const flyingControls = new FlyControls(camera, renderer.domElement);
+
+camera.position.z = 10;
 
 const animate = () => {
   requestAnimationFrame(animate)
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  controls.update();
+  cubes.forEach(cube => {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+  })
+  flyingControls.update(0.5);
   renderer.render(scene, camera)
 }
 animate();
