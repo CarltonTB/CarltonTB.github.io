10968227c0248ba96b238b1f4c023893ab5c0f8e
Carlton Brady <ctombrady@gmail.com>
2025-05-26 10:37:24 -0400

    deploy

--- personal-site/src/main.ts
+++ personal-site/src/main.ts
@@ -1,9 +1,10 @@
 const getYearsOfExperience = (): string => {
-  const startYear = 2017;
-  const currentYear = new Date().getFullYear();
-  const yoe: number = currentYear - startYear;
-  return `${yoe} years`;
+    const startYear = 2017;
+    const currentYear = new Date().getFullYear();
+    const yoe: number = currentYear - startYear;
+    return `${yoe} years`;
 }
 
 document.getElementById('yearsOfExperience')!.textContent = getYearsOfExperience();
+
 export { }
